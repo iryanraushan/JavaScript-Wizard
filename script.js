369ce81e1710422d91cacd067b70aea8b1711a1e
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-02-04 22:18:48 +0530

    JavaScript - Day - 01

---
+++ script.js
@@ -0,0 +1,97 @@
+// console.log("Hello")
+// console.log("Hello")
+
+// // variable
+// var myName = "Insane Ryan"
+// console.log(myName)
+
+// var myAge = 20;
+// console.log(myAge)
+
+// // We can use  '_' , '$' , and letter as a first letter.
+// // we can't use numeric as a 1st character
+// // No keyword use as a variable name.
+
+// var $name = '76'; // can create.
+// // var 7color = "Rainbow" // can't create.
+// console.log($name)
+
+// // Note : we can use semicolo ":" or not doesn't matter.
+
+// // Data Types in Javascript :
+// //     undefined
+// //     Boolean
+// //     Number
+// //     String
+// //     BigInt
+// //     Symbol
+
+// // typeof operator ==> use to know datatype
+// var stringType = "i am ryan";
+// var numberType = 87;
+// var booleanType = false;
+// var bigIntType = 876648326875555;
+// var undefinedType; // undefined type.
+// console.log(undefinedType, typeof(undefinedType))
+// console.log(stringType);
+// console.log(numberType);
+// console.log(booleanType);
+// console.log(bigIntType);
+
+// var floatValue = 765.86; //NUmber type.
+// console.log(floatValue, typeof(floatValue));
+
+// // Opearations
+
+// console.log(10 + "30", typeof (10 + "30")); //1030 string
+// console.log(9 - "5", typeof (9 - "5")); // 4 number
+// console.log("Java" + "Script", typeof ("Java" + "Script")); // JavaScript string
+// console.log(" " + " ", typeof (" " + " ")); // string
+// console.log("vinod" - "thapa", typeof ("vinod" - "thapa")); // NaN number
+// console.log(true + true, typeof (true + true)); // 2 number
+// console.log(true + false, typeof (true + false)); // 1 number
+// console.log(false + false, typeof (false + false)); // 0 number
+// console.log(true - true, typeof (true - true)); // 0 number
+// console.log(false - true, typeof (false - true)); // -1 number
+
+// // Diffrence between null and undefined
+
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless));
+
+// var iAmStandBy;
+// console.log(iAmStandBy);
+// console.log(typeof(iAmStandBy));
+
+// var myPhoneNumber = 8434290086
+// var myName = 'insane ryan'
+
+// console.log(myPhoneNumber)
+// console.log(myName)
+
+// console.log(isNaN(myPhoneNumber))
+// console.log(isNaN(myName))
+
+// if (!isNaN(myPhoneNumber)){
+//     console.log("PLease Enter a valid phone number!!")
+// }
+
+// console.log(NaN === NaN); // return false
+
+
+        /* Assignment Operator */
+
+// 1. Addition Assignment  (x += y) ,  (x = x + y)
+// 2. Division Assignment  (x /= y )   (x = x / y)
+// 3. Exponentiation Assignment   (x **= y)   (x = x ** y)
+// 4. Right Shift Assignment   (x >>= y)    (x = x >> y)
+// 5. Bitwise XOR Assignment   (x ^= y)    (x = x ^ y)
+
+
+// // (===) operator used to compare the two value known as strict equality operator. It checks both data and the content. if they are not equal return false.
+// console.log("1" === 1); // return false
+
+// // The equality operator (==) is used to compare the two values, if they are equal or not.But the values are not directly compared. First,they are converted to the same data type and then the converted content is compared.
+// console.log("1" == "1");
+
