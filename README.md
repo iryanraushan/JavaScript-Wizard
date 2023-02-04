1a462f9e50170b5fd918aa242561357bd228f69e
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-02-04 23:11:53 +0530

    js

--- README.md
+++ README.md
@@ -21,3 +21,33 @@ console.log('Hello World!');
 Hello World!
 
 ```
+
+# Variable And Datatypes
+
+### There Are 7 data types available in JavaScript.
+
+#### Primitive
+    - Number - Represents integer and floating values.
+    - String - Represents Textual data.
+    - Boolean - Logical entity with values as true or false.
+    - Undefined - Represents a variable whose value is not yet assigned.
+    - Null - Represents the intentional absence of value.
+    - Symbol - Represent a unique value.
+
+#### Non-Primitive
+    - Object - Represents Key-value pair.
+
+### Variables
+variables are named containers for storing data.
+
+#### 3 types to declare a JavaScript variable.
+    - Using **var**
+    - Using **let**
+    - Using **const**
+
+### Variable Declaration
+
+```
+var temp
+```
+*Here temp is the variable name without any value. By default, it assigned with **undefined***
--- script.js
+++ script.js
@@ -5,3 +5,43 @@
 // alert("Hii.. I just start learning javascript.");
 
 // console.log('Hello World!');
+
+// variable
+// var myName = "Insane Ryan"
+// console.log(myName)
+
+// var myAge = 20;
+// console.log(myAge)
+
+// // We can use  '_' , '$' , and letter as a first letter.
+// // we can't use numeric as a 1st character
+// // No keyword use as a variable name.
+
+// var $name = '76'; // can create.
+// // var 7color = "Rainbow" // can't create.
+// console.log($name)
+
+// // Note : we can use semicolo ":" or not doesn't matter.
+
+// // Data Types in Javascript :
+// //     undefined
+// //     Boolean
+// //     Number
+// //     String
+// //     BigInt
+// //     Symbol
+
+// // typeof operator ==> use to know datatype
+// var stringType = "i am ryan";
+// var numberType = 87;
+// var booleanType = false;
+// var bigIntType = 876648326875555;
+// var undefinedType; // undefined type.
+// console.log(undefinedType, typeof(undefinedType))
+// console.log(stringType);
+// console.log(numberType);
+// console.log(booleanType);
+// console.log(bigIntType);
+
+// var floatValue = 765.86; //NUmber type.
+// console.log(floatValue, typeof(floatValue));
