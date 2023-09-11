ae6f4c2490555a90d1cc687187973caf735ece5e
Ryan Raushan <raushankrpnhs2000@gmail.com>
2023-09-11 18:25:04 +0530

    Removed Some Garbage

--- js/comparisonOperator.js
+++
@@ -1,8 +0,0 @@
-// ==	Is equal to
-// ===	Identical (equal and of same type)
-// !=	Not equal to
-// !==	Not Identical
-// >	Greater than
-// >=	Greater than or equal to	
-// <	Less than	
-// <=	Less than or equal to	
--- js/table.js
+++
@@ -1,6 +0,0 @@
-let a = "ryna"
-var v = "komal"
-s = "harsha"
-b = "Ryan@gmail.com"
-
-console.table([a, v, s, b])
